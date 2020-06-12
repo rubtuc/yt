dd3fac5386585d2c86d26e1c54c79241c1389a3b
hmm <937380166@qq.com>
2020-06-12 23:39:53 +0800

    首页气泡

---
+++ src/views/Map/MapSecond.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="body">
+      <div style="display: flex;margin-top: 8px">
+        <img src="../../../images/返 回.png" class="back" @click="backToMap">
+        <div class="second-search">
+          <div>
+            <div class="flex-row" >
+              <div class="start-point"></div>
+              <input class="input" placeholder="我的位置" id="tipinput" style="border-bottom: 2px solid #95959545">
+            </div>
+            <div class="flex-row">
+              <div class="end-point"></div>
+              <input class="input" placeholder="目的地" id="tipinput1" style="position: relative">
+            </div>
+          </div>
+          <img src="../../../images/交换.png" class="reverse-icon">
+        </div>
+
+      </div>
+      <div class="route-section">
+        <div v-for="item in recomendList" :key="item.id" style="height: 75px">
+          <div class="station">
+            <div class="station-num">{{item.from}}号线</div>
+            <img src="../../../images/右.png" style="width: 9px;
+	height: 13px;margin: 0 7px">
+            <div class="station-num">{{item.to}}号线</div>
+          </div>
+          <p class="recomend-info">{{item.time}}*{{item.price}}*{{item.meter}}</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import { Trip } from '@/api/trip';
+    export default {
+      name: "MapSecond",
+      created(){
+        const Buttons = this.$refs.recordNavButton.getElementsByTagName('button');
+      },
+      data() {
+        return {
+          recomendList: [{
+            id: 0,
+            from: "1",
+            to: "2",
+            time: "1.5h",
+            meter: "10.9km",
+            price: "6元"
+          },
+            {
+              id: 1,
+              from: "1",
+              to: "2",
+              time: "1.5h",
+              meter: "10.9km",
+              price: "6元"
+            },
+            {
+              id: 1,
+              from: "1",
+              to: "2",
+              time: "1.5h",
+              meter: "10.9km",
+              price: "6元"
+            }],
+        }
+      },
+      methods:{
+        backToMap(){
+          this.$router.push('/map')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .back{
+    width: 8px;
+    height: 12px;margin-left: 4%;margin-right:11px;margin-top:19px;
+  }
+  .recomend-info{
+    text-align: left;
+    font-size: 12px;
+    color: #818181;
+  }
+  .station-num{
+    width: 55px;
+    height: 25px;
+    border-radius: 13px;
+    font-size: 13px;
+    color: #ffffff;
+    background-color: #00c62f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .station{
+    display: flex;
+    align-items: center;
+    margin: 0;
+  }
+  .route-section{
+    margin-top: 11px;
+    margin-left: 8.2%;
+    height: 75px;
+    padding: 15px 0 15px 0;
+  }
+  .body{
+    width: 92%;
+  }
+  .second-search{
+    flex: 1;
+    background-color: #ebe7e7;
+    border-radius: 10px;
+    opacity: 0.43;
+    display: flex;
+    padding: 0 22px 0 12px;
+    justify-content: center;
+    align-items: center;
+  }
+  .flex-row{
+    display: flex;
+    align-items: center;
+  }
+  .search{
+    z-index: 1;
+    position: absolute;
+    right: 75px;
+  }
+  .start-point{
+    width: 8px;
+    height: 8px;
+    background-color: #87c67d;
+    border-radius: 50%;
+  }
+  .end-point{
+    width: 8px;
+    height: 8px;
+    background-color: #f2404d;
+    border-radius: 50%;
+  }
+  .input{
+    width: 249px;
+    margin: 10px 0px 0px 13px;
+    border:none;
+    outline:medium;
+    padding: 0 0 10px 0;
+    background-color: transparent;
+  }
+  .reverse-icon{
+    width: 17px;
+    height: 16px;
+    border-radius: 50%;
+    margin: 0 0 12px 14px;
+  }
+</style>
---
+++ src/views/Record.vue
@@ -0,0 +1,103 @@
+<template>
+  <div style="background-color: #f6f6f6;height: 100vh">
+<!--    <div class="nav">-->
+<!--      <p class="back" @click="backToMap"><返回</p>-->
+<!--      <p class="nav-center">行程记录</p>-->
+<!--    </div>-->
+    <div class="shop-nav">
+      <p class="back" @click="backToMap">返回</p>
+      <p class="nav-center">行程记录</p>
+    </div>
+  <div v-for="item in HistoryList" :key="item.trip_no" style="background-color: white">
+    <div style="display: flex;justify-content: flex-start;margin-left: 4%" >
+      <div style="text-align: center;">
+        <img src="../../images/进.png" style="width: 22px;height: 22px;margin: 20px 0;">
+      </div>
+      <van-cell :title="item.in_station_name"  size="large" :label="item.in_time" />
+    </div>
+    <div style="display: flex;justify-content: flex-start;margin-left: 4%" >
+      <div style="text-align: center;">
+        <img src="../../images/出.png" style="width: 22px;height: 22px;margin: 20px 0;">
+      </div>
+      <van-cell :title="item.out_station_name"  size="large" :label="item.out_time" />
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import { Trip } from '../api/trip'
+    export default {
+        name: "Record",
+      data(){
+          return{
+            HistoryList:[
+            //   {
+            //   id:0,
+            //   station_in: "杭州东站",
+            //   routeInfo_in: "2019-10-23 17:54:23",
+            //   station_out: "杭州东站",
+            //   routeInfo_out: "2019-10-23 17:54:23",
+            // },
+              // {
+              //   id:0,
+              //   station: "杭州东站",
+              //   routeInfo: "2019-10-23 17:54:23",
+              //   url: require('../../images/出.png')
+              // }
+              ]
+          }
+      },
+      created(){
+          Trip().then((res)=>{
+            console.log(res);
+              this.HistoryList.push(...res);
+          })
+      },
+      methods:{
+        backToMap(){
+          this.$router.push('/map')
+        },
+        goBack(){
+          this.$router.go(-1);
+        }
+      }
+    }
+</script>
+
+<style scoped lang="less">
+  .van-cell--large .van-cell__title {
+    font-size: 16px;
+    text-align: left;
+  }
+  .van-icon__image {
+    width: 22px;
+    height: 22px;
+    /* object-fit: contain; */
+  }
+  .van-cell__title, .van-cell__value {
+     -webkit-box-flex: 1;
+     -webkit-flex: 1;
+     flex: none;
+  }
+  .back{
+    font-size: 12px;
+    margin: 17px 0 13px 13px;
+    color: #4271dc;
+  }
+  .shop-nav{
+    background-color: #f6f6f6;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+  }
+  .nav-center{
+    position: absolute;
+    text-align: center;
+    width: 100%;
+    font-size: 14px;
+    color: #000000;
+    font-weight: bold;
+  }
+</style>
